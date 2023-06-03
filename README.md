ee5c258d78986ec1d543b8d411f55ee457c97593
E. Huynh <elh7@rice.edu>
2023-06-03 13:25:08 -0500

    Update backend.py
    Developed organizational method for document types (collaborations, theses, and related publications).

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 Experimenting with INSPIRE's REST API to automate the addition of DUNE-related publications to a designated site.
 
 ## To Do
-- Develop organizational method for different types of publications
-- Decide on formatting for different types of publications
-- If appropriate, create conditionals for edge cases
-- Figure out how to connect the backend and frontend
+- Figure out how to get IDs/links
+- Connect backend to a frontend
+- Consider benefits of class wrapping
--- backend.py
+++ backend.py
@@ -1,19 +1,46 @@
 # =======
 # IMPORTS
 
+from datetime import datetime
 import requests
 
 # ============
 # PAGE REQUEST
 
-ENTRIES = requests.get("https://inspirehep.net/api/literature?sort=mostrecent&size=1000&page=1&q=accelerator_experiments.record.%24ref%3A1346343").json()["hits"]["hits"]
-f_entries = []
+def get_id(entry: dict):
+    pass
 
-for i in range(len(1)): # Iterate through and format entries
-    c_entry = ENTRIES[i]
-    c_author = c_entry["metadata"]["authors"][0]["first_name"] + " " + c_entry["metadata"]["authors"][0]["last_name"] + " et al., "
-    c_title = "\"" + c_entry["metadata"]["titles"][0]["title"] + "\", "
-    c_arxiv = "arXiv:" + c_entry["metadata"]["arxiv_eprints"][0]["value"]
-    f_entries.append(c_author + c_title + c_arxiv)
+def get_collab_info(entry: dict):
+    author = "DUNE collaboration"
+    title = "\"" + entry["titles"][0]["title"] + "\""
+    return [author, title]
 
-print(f_entries)
+def get_thesis_info(entry: dict):
+    author = " ".join(entry["authors"][0]["full_name"].split(", ")[::-1])
+    title = entry["titles"][0]["title"]
+    institution = entry["thesis_info"]["institutions"][0]["name"]
+    if institution[-2:] == "U.":
+        institution = institution[:-2] + "University"
+    date = datetime.strptime(entry["thesis_info"]["date"], "%Y-%m").strftime("%B %Y")
+    return [author, title, institution, "Ph.D.", date]
+
+def get_related_info(entry: dict):
+    author = " ".join(entry["authors"][0]["full_name"].split(", ")[::-1]) + " et al."
+    title = "\"" + entry["titles"][0]["title"] + "\""
+    return [author, title]
+
+def make_query(page: str) -> dict:
+    entries = requests.get(page).json()["hits"]["hits"]
+    publications = {"collab": [], "thesis": [], "related": []}
+    for i in range(10): # Iterate through and format entries
+        c_entry = entries[i]["metadata"]
+        if "collaborations" in c_entry and "DUNE" in c_entry["collaborations"][0]["value"]: # Publication by DUNE Collaboration
+            raw = get_collab_info(c_entry)
+            publications["collab"].append(", ".join(raw))
+        elif "thesis" in c_entry["document_type"]: # DUNE Thesis
+            raw = get_thesis_info(c_entry)
+            publications["thesis"].append(", ".join(raw))
+        else: # Related publications by DUNE Collaborators
+            raw = get_related_info(c_entry)
+            publications["related"].append(", ".join(raw))
+    return publications
