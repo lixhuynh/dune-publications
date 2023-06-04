09f8c19f63c9a969d5821ccc640da837e9a4869b
E. Huynh <elh7@rice.edu>
2023-06-04 13:45:00 -0500

    Updated backend.py
    Implemented a rudimentary get_id method. Accounted for certain edge cases.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Experimenting with INSPIRE's REST API to automate the addition of DUNE-related publications to a designated site.
 
 ## To Do
-- Figure out how to get IDs/links
+- Finalize get_id method
+- Figure out how to segment query
 - Connect backend to a frontend
 - Consider benefits of class wrapping
--- backend.py
+++ backend.py
@@ -8,33 +8,54 @@ import requests
 # PAGE REQUEST
 
 def get_id(entry: dict):
-    pass
+    identifier = entry["control_number"]
+    return "https://inspirehep.net/literature/" + str(identifier)
 
 def get_collab_info(entry: dict):
     author = "DUNE collaboration"
     title = "\"" + entry["titles"][0]["title"] + "\""
-    return [author, title]
+    id = get_id(entry)
+    return [author, title, id]
 
 def get_thesis_info(entry: dict):
     author = " ".join(entry["authors"][0]["full_name"].split(", ")[::-1])
     title = entry["titles"][0]["title"]
     institution = entry["thesis_info"]["institutions"][0]["name"]
-    if institution[-2:] == "U.":
-        institution = institution[:-2] + "University"
-    date = datetime.strptime(entry["thesis_info"]["date"], "%Y-%m").strftime("%B %Y")
-    return [author, title, institution, "Ph.D.", date]
+    # Formatting institution as appropriate
+    if institution[:2] == "U.":
+        institution = "University of" + institution[2:]
+    elif "U." in institution:
+        institution = institution.replace("U.", "University")
+    elif institution[:5] == "Coll.":
+        institution = "College of" + institution[5:]
+    elif "Coll." in institution:
+        institution = institution.replace("Coll.", "College")
+    # Formatting date as appropriate
+    raw_date = entry["earliest_date"]
+    if len(raw_date) == 4:
+        date = datetime.strptime(raw_date, "%Y").strftime("%Y")
+    elif len(raw_date) == 7:
+        date = datetime.strptime(raw_date, "%Y-%m").strftime("%B %Y")
+    elif len(raw_date) == 10:
+        date = datetime.strptime(raw_date, "%Y-%m-%d").strftime("%B %Y")
+    else:
+        date = "Unknown"
+    id = get_id(entry)
+    return [author, title, institution, "Ph.D.", date, id]
 
 def get_related_info(entry: dict):
     author = " ".join(entry["authors"][0]["full_name"].split(", ")[::-1]) + " et al."
     title = "\"" + entry["titles"][0]["title"] + "\""
-    return [author, title]
+    id = get_id(entry)
+    return [author, title, id]
 
 def make_query(page: str) -> dict:
     entries = requests.get(page).json()["hits"]["hits"]
     publications = {"collab": [], "thesis": [], "related": []}
-    for i in range(10): # Iterate through and format entries
+    for i in range(len(entries)): # Iterate through and format entries
         c_entry = entries[i]["metadata"]
-        if "collaborations" in c_entry and "DUNE" in c_entry["collaborations"][0]["value"]: # Publication by DUNE Collaboration
+        if "collaborations" in c_entry and "DUNE" in [collab['value'].upper() for collab in c_entry["collaborations"]]:
+            # Publication by DUNE Collaboration
             raw = get_collab_info(c_entry)
             publications["collab"].append(", ".join(raw))
         elif "thesis" in c_entry["document_type"]: # DUNE Thesis
@@ -44,3 +65,5 @@ def make_query(page: str) -> dict:
             raw = get_related_info(c_entry)
             publications["related"].append(", ".join(raw))
     return publications
+
+output = make_query("https://inspirehep.net/api/literature?sort=mostrecent&size=188&page=1&q=accelerator_experiments.record.%24ref%3A1346343")
