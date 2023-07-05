a84a1f75b9881d9e7f51d8448d65243c0ee4cbf3
lixhuynh <elh7@rice.edu>
2023-07-05 17:21:18 -0500

    Remove related publications & theses

--- scripts/backend.py
+++ scripts/backend.py
@@ -1,7 +1,6 @@
 # =======
 # IMPORTS
 
-from datetime import datetime
 import requests
 
 # ============
@@ -10,11 +9,7 @@ import requests
 def get_id(entry: dict) -> list:
     """
     Gets and formats the links to a particular entry.
-    """
-    if "thesis" in entry["document_type"]:
-        # Theses require unique formatting
-        return ["https://inspirehep.net/literature/" + str(entry["control_number"])]
-    
+    """    
     links = []
     if "arxiv_eprints" in entry:
         id = str(entry["arxiv_eprints"][0]["value"])
@@ -38,42 +33,6 @@ def get_collab_info(entry: dict) -> list:
     title = "\"" + entry["titles"][0]["title"] + "\""
     return [author, title] + get_id(entry)
 
-def get_thesis_info(entry: dict) -> list:
-    """
-    Gets the information for a thesis entry and enters it into a list.
-    """
-    author = " ".join(entry["authors"][0]["full_name"].split(", ")[::-1])
-    title = "<a href=" + get_id(entry)[0] + ">" + entry["titles"][0]["title"] + "</a>"
-    institution = entry["thesis_info"]["institutions"][0]["name"]
-    # Formatting institution as appropriate
-    if institution[:2] == "U.":
-        institution = "University of" + institution[2:]
-    elif "U." in institution:
-        institution = institution.replace("U.", "University")
-    elif institution[:5] == "Coll.":
-        institution = "College of" + institution[5:]
-    elif "Coll." in institution:
-        institution = institution.replace("Coll.", "College")
-    # Formatting date as appropriate
-    raw_date = entry["earliest_date"]
-    if len(raw_date) == 4:
-        date = datetime.strptime(raw_date, "%Y").strftime("%Y")
-    elif len(raw_date) == 7:
-        date = datetime.strptime(raw_date, "%Y-%m").strftime("%B %Y")
-    elif len(raw_date) == 10:
-        date = datetime.strptime(raw_date, "%Y-%m-%d").strftime("%B %Y")
-    else:
-        date = "Unknown"
-    return [author, title, institution, "Ph.D.", date]
-
-def get_related_info(entry: dict) -> list:
-    """
-    Gets the information for a related entry and enters it into a list.
-    """
-    author = " ".join(entry["authors"][0]["full_name"].split(", ")[::-1]) + " <i>et al.</i>"
-    title = "\"" + entry["titles"][0]["title"] + "\""
-    return [author, title] + get_id(entry)
-
 def retrieve_metadata(page: str) -> dict:
     """
     Returns the metadata for a single entry.
@@ -81,28 +40,20 @@ def retrieve_metadata(page: str) -> dict:
     entry = requests.get(page).json()["metadata"]
     return entry
 
-def make_query(page: str) -> dict:
+def make_query(page: str) -> list:
     """
     Sorts the entries on a given page into collaborations, theses, and related publications.
     Places them into a format ready for HTML tagging.
     """
     entries = requests.get(page).json()["hits"]["hits"]
-    publications = {"collab": [], "thesis": [], "related": []}
+    publications = []
     for i in range(len(entries)): # Iterate through and format entries
         c_entry = entries[i]["metadata"]
-        if "conference paper" in c_entry["document_type"]:
-            # We ignore conference papers for the purpose of this page
-            continue
-        if "collaborations" in c_entry and "DUNE" in [collab['value'].upper() for collab in c_entry["collaborations"]]:
-            # Publication by DUNE Collaboration
+        if "conference paper" not in c_entry["document_type"] \
+            and "collaborations" in c_entry \
+                and "DUNE" in [collab['value'].upper() for collab in c_entry["collaborations"]]:
             raw = get_collab_info(c_entry)
-            publications["collab"].append(", ".join(raw))
-        elif "thesis" in c_entry["document_type"]: # DUNE Thesis
-            raw = get_thesis_info(c_entry)
-            publications["thesis"].append(", ".join(raw))
-        else: # Related publications by DUNE Collaborators
-            raw = get_related_info(c_entry)
-            publications["related"].append(", ".join(raw))
+            publications.append(", ".join(raw))
     return publications
 
 def get_raw_html(publications: dict) -> str:
@@ -110,13 +61,7 @@ def get_raw_html(publications: dict) -> str:
     Converts the result of running make_query into a string representing the raw HTML file.
     """
     formatted = "<h2>Publications/Documents by the DUNE Collaboration</h2><ul>"
-    for item in publications["collab"]:
-        formatted += "<li>" + item + "</li>"
-    formatted += "</ul><h2>DUNE Theses</h2><ul>"
-    for item in publications["thesis"]:
-        formatted += "<li>" + item + "</li>"
-    formatted += "</ul><h2>Related Publications by DUNE Collaborators</h2><ul>"
-    for item in publications["related"]:
+    for item in publications:
         formatted += "<li>" + item + "</li>"
     formatted += "</ul>"
     return formatted
@@ -129,7 +74,7 @@ def format_html(page_content: str, page_title: str, template_name: str) -> str:
         template = file.read()
     title_start = template.find("entry-title") + 13
     title_end = template.find("</h1>", title_start)
-    content_start = template.find("entry-content") + 15
+    content_start = template.find("entry-content", title_end) + 15
     content_end = template.find("</div>", content_start)
     formatted = template[:title_start] + page_title + template[title_end:content_start] + page_content + template[content_end:]
     return formatted
