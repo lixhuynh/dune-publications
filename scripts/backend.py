9933bf3c1a13c6913345aed91cec490277173fa0
lixhuynh <elh7@rice.edu>
2023-07-05 17:06:40 -0500

    Auto-format HTML
    Added a function to automatically format the HTML file based on a given template. Also reorganized file structure so it seems cleaner.

--- scripts/backend.py
+++ scripts/backend.py
@@ -105,11 +105,11 @@ def make_query(page: str) -> dict:
             publications["related"].append(", ".join(raw))
     return publications
 
-def format_query(publications: dict) -> str:
+def get_raw_html(publications: dict) -> str:
     """
-    Formats the dictionary resulting from make_query.
+    Converts the result of running make_query into a string representing the raw HTML file.
     """
-    formatted = "<h1>Documents and Publications</h1><h2>Publications/Documents by the DUNE Collaboration</h2><ul>"
+    formatted = "<h2>Publications/Documents by the DUNE Collaboration</h2><ul>"
     for item in publications["collab"]:
         formatted += "<li>" + item + "</li>"
     formatted += "</ul><h2>DUNE Theses</h2><ul>"
@@ -121,6 +121,20 @@ def format_query(publications: dict) -> str:
     formatted += "</ul>"
     return formatted
 
+def format_html(page_content: str, page_title: str, template_name: str) -> str:
+    """
+    Takes the raw HTML and puts it into the given template.
+    """
+    with open(template_name, "r") as file:
+        template = file.read()
+    title_start = template.find("entry-title") + 13
+    title_end = template.find("</h1>", title_start)
+    content_start = template.find("entry-content") + 15
+    content_end = template.find("</div>", content_start)
+    formatted = template[:title_start] + page_title + template[title_end:content_start] + page_content + template[content_end:]
+    return formatted
+
 output = make_query("https://inspirehep.net/api/literature?sort=mostrecent&size=188&page=1&q=accelerator_experiments.record.%24ref%3A1346343")
-formatted = format_query(output)
+raw = get_raw_html(output)
+formatted = format_html(raw, "Documents and Publications", "template.html")
 print(formatted)
--- scripts/exporter.py
+++ scripts/exporter.py
@@ -1,4 +1,4 @@
 import subprocess
 
-with open("index.html", "w+") as output:
+with open("../index.html", "w+") as output:
     subprocess.call(["python", "./backend.py"], stdout=output)
