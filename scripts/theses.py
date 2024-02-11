7ba7540df0768ce70a011d3fb31f29b42545fcdd
lixhuynh <elh7@rice.edu>
2024-02-10 19:37:21 -0600

    v3.1 - finished theses script

--- scripts/theses.py
+++ scripts/theses.py
@@ -1,8 +1,13 @@
+import re
 import requests
+from datetime import datetime
 
-DUNE_THESES = "https://inspirehep.net/api/literature?sort=mostrecent&size=188&page=1&collaboration=DUNE"
+DUNE_THESES = "https://inspirehep.net/api/literature?sort=mostrecent&size=200&page=1&q=accelerator_experiments.record.%24ref%3A1346343"
 
 def make_query(page: str) -> list:
+    """
+    Makes a request to INSPIRE for the entries.
+    """
     entries = requests.get(page).json()["hits"]["hits"]
     theses = []
     for i in range(len(entries)):
@@ -14,7 +19,40 @@ def make_query(page: str) -> list:
                     found_match = True
                     break
             if found_match:
-                theses.append(c_entry["titles"][0]["title"])
+                theses.append(format_entry(c_entry))
     return theses
 
-print(make_query(DUNE_THESES))
+def format_entry(entry: dict) -> str:
+    """
+    Formats an entry.
+    """
+    name = f'{entry["first_author"]["first_name"]} {entry["first_author"]["last_name"]}'
+    title = entry["titles"][0]["title"]
+    url = entry["urls"][0]["value"]
+    degree = entry["thesis_info"]["degree_type"]
+    university = entry["thesis_info"]["institutions"][0]["name"]
+    date = format_date(entry["thesis_info"]["date"])
+
+    return f'{name}, <a href="{url}">{title}</a>, {university}, {degree}, {date}'
+
+def format_date(input_date: str) -> str:
+    """
+    Formates a publication date.
+    """
+    if re.match("\d{4}-\d{2}", input_date): # YYYY-MM
+        date = datetime.strptime(input_date, '%Y-%m').strftime('%B %Y')
+    else:
+        date = input_date
+    return date
+
+def get_raw_html(header: str, items: list) -> str:
+    """
+    Converts the result of running make_query into a string representing the raw HTML file.
+    """
+    formatted = f"<h2>{header}</h2>\n<ul>\n"
+    for item in items: formatted += f"\t<li>{item}</li>\n"
+    formatted += "</ul>"
+    return formatted
+
+with open("./theses.html", mode="w") as output:
+    print(get_raw_html("DUNE Theses", make_query(DUNE_THESES)), file=output)
